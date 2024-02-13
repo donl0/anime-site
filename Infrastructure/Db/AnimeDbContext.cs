779b538bb6ca876c2b2e2362e214c8691a4fd8cd
donl0 <donl_117n@mail.ru>
2024-02-13 23:06:17 +0300

    Rework folders

---
+++ Infrastructure/Db/AnimeDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Models.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Db
+{
+    public sealed class AnimeDbContext : DbContext, IAnimeDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rating> Ratings { get; }
+        public DbSet<Bookmark> Bookmarks { get; }
+
+        public AnimeDbContext(DbContextOptions<AnimeDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}

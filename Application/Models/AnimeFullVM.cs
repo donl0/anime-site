8f552895343ac408bd9f240874af5ae583c2c455
donl0 <donl_117n@mail.ru>
2024-02-14 19:47:16 +0300

    Add models for anime logic view

---
+++ Application/Models/AnimeFullVM.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models.Shiki;
+
+namespace Application.Models
+{
+	public class AnimeFullVM
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public Image Image { get; set; }
+		public Genre[] Genres { get; set; }
+		public string Score { get; set; }
+		public int Episodes { get; set; }
+        public string Kind { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ Application/Models/AnimePageVM.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models.Shiki;
+
+namespace Application.Models
+{
+	public class AnimePageVM
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public Image Image { get; set; }
+		public string Score { get; set; }
+	}
+}

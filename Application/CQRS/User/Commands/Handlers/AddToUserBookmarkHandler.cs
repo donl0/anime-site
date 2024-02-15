aff3fa7e62bbce0ad5e9f1e7523743036140c496
donl0 <donl_117n@mail.ru>
2024-02-16 00:39:01 +0300

    Separate on folders

---
+++ Application/CQRS/User/Commands/Handlers/AddToUserBookmarkHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.CQRS.User.Commands;
+using Application.CQRS.User.Commands.Handlers.Common;
+using Application.Interfaces;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.User.Commands.Handlers
+{
+    internal class AddToUserBookmarkHandler : UserBookmarkJobber, IRequestHandler<AddToUserBookmarkCommand, bool>
+    {
+        private readonly IAnimeDbContext _context;
+
+        public AddToUserBookmarkHandler(IAnimeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(AddToUserBookmarkCommand request, CancellationToken cancellationToken)
+        {
+            Domain.Models.User.Bookmark bookmark = await TryTakeBookmark(_context, request.AnimeId, request.UserId, request.Bookmark);
+
+            Domain.Models.Anime anime = new Domain.Models.Anime
+            {
+                Id = request.AnimeId
+            };
+
+            bookmark.Animes.Add(anime);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
---
+++ Application/CQRS/User/Commands/Handlers/Common/UserBookmarkJobber.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.User.Commands.Handlers.Common
+{
+    public class UserBookmarkJobber
+    {
+        protected async Task<Domain.Models.User.Bookmark> TryTakeBookmark(IAnimeDbContext context, int animeId, string userId, string bookmarkName)
+        {
+
+            var user = await context.Users.SingleOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+                throw new NotFoundException(nameof(User), bookmarkName);
+
+            var bookmark = await context.Bookmarks.SingleOrDefaultAsync(b => b.Title == bookmarkName & b.User == user);
+
+            if (bookmark == null)
+                throw new NotFoundException(nameof(Domain.Models.User.Bookmark), bookmark);
+
+            return bookmark;
+        }
+    }
+}

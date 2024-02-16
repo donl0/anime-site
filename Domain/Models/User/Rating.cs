71fc9e876e8f433553a9c4e583afa874ef68f3de
donl0 <donl_117n@mail.ru>
2024-02-16 16:10:22 +0300

    Change from anime model to int

--- Domain/Models/User/Rating.cs
+++ Domain/Models/User/Rating.cs
@@ -3,7 +3,7 @@
 	public class Rating
     {
         public int Id { get; set; }
-        public Anime Anime { get; set; }
+        public int Anime { get; set; }
         public User User { get; set; }
         public int value { get; set; }
     }
